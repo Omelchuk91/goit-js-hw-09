8072ed612b2f23099031bcf279d731c861286d6d
Roman Omelchuk <roman.omelchuk91@gmail.com>
2022-10-08 12:19:51 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let randomHexColor = null;
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+refs.stopBtn.setAttribute('disabled', '');
+
+function onStartBtnClick() {
+  refs.stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', '');
+
+  intervalId = setInterval(() => changeBodyBgColor(), 1000);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', '');
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBgColor() {
+  randomHexColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = randomHexColor;
+}
